e89fb44263a2525ed8bbfc5d320827266bd6243d
Vladislav Korobeinikov <vladkorobeinikov@gmail.com>
2023-12-03 03:12:56 +0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from "./components/Search";
+
+
 const App = () => {
+    const [word, setWord] = useState('')
+
+    const handeSearchSubmit = (e) => {
+        e.preventDefault();
+        console.log(word)
+    }
+
   return (
     <div className="App">
         <Header title={"Images gallery"}/>
+        <Search word={word} setWord={setWord} handeSubmit={handeSearchSubmit}/>
     </div>
   );
 }
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
+
+
+const Search = ({word, setWord, handeSubmit}) => {
+    return (
+        <Container className="mt-4">
+            <Form onSubmit={handeSubmit}>
+                <Col xs={12} md={8} lg={6}>
+                    <Row className="justify-content-center">
+                        <Col xs={9}>
+                            <Form.Control
+                                placeholder="search"
+                                type="text"
+                                value={word}
+                                onChange={(e) => setWord(e.target.value)}
+                            />
+                        </Col>
+                        <Col>
+                            <Button variant="primary" type="submit">Search</Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Form>
+        </Container>
+    );
+};
+
+export default Search;
