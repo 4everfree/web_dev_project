bbe0fc4218868222666e274aa7b506219967a63d
vladislavkorobeinikov <vladkorobeinikov@gmail.com>
2023-12-25 03:47:03 +0400

    added post get for images

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
 import requests
-import pymongo
 import os
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from mongo_client import mongo_client
 
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path='.env.local')
 UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
@@ -12,7 +14,7 @@ UNSPLASH_KEY = os.environ.get('UNSPLASH_KEY', "")
 DEBUG = bool(os.environ.get('DEBUG', False))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
+    raise EnvironmentError("Please  create .env.local file and insert there UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
@@ -29,6 +31,15 @@ def new_image():
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
+@app.route('/images', methods=['GET','POST'])
+def images():
+    if request.method == 'GET':
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+    if request.method == 'POST':
+        image = request.get_json()
+        return images_collection.insert_one(image)
 
 
 if __name__ == "__main__":
