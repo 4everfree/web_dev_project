94b4955de6496e9e6dbda9e0ee63c903916ac748
vladislavkorobeinikov <vladkorobeinikov@gmail.com>
2023-12-27 03:02:08 +0400

    added useEffect function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Search from "./components/Search";
 import ImageCard from "./components/ImageCard";
 import {Container, Row, Col} from 'react-bootstrap';
-import React from 'react'
+import React from 'react';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050'
 
 const App = () => {
     const [word, setWord] = useState('');
     const [images, setImages] = useState([]);
+
+    const getSavedImages = async () => {
+        try {
+            const res = await axios.get(`${API_URL}/images`);
+            setImages(res.data || []);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(()=> getSavedImages(), []);
 
     const handeSearchSubmit = async (e) => {
         e.preventDefault();
